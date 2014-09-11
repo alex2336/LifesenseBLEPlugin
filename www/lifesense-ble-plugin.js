23e40ea8d4612d8fd214927708fff549fe7a9ce5
alex2336 <me@xianqun.net>
2014-09-11 12:30:03 +0800

    Update lifesense-ble-plugin.js

--- www/lifesense-ble-plugin.js
+++ www/lifesense-ble-plugin.js
@@ -1,85 +1,86 @@
 var lifesenseBLEPlugin = {
-    startScanning: function(successCallback, errorCallback) {
-        cordova.exec(
-            successCallback, // success callback function
-            errorCallback, // error callback function
-            'LifesenseBLEPlugin', // mapped to our native Java class called "LifesenseBLEPlugin"
-            'startScanning', // with this action name
-            [{}]
-        ); 
-     },
-     stopScanning: function(successCallback, errorCallback) {
-        cordova.exec(
-            successCallback, 
-            errorCallback, 
-            'LifesenseBLEPlugin',
-            'stopScanning',
-            [{}]
-        ); 
-     },
-     getCurrentDevices:function(successCallback, errorCallback) {
-        cordova.exec(
-            successCallback, 
-            errorCallback, 
-            'LifesenseBLEPlugin', 
-            'getCurrentDevices',
-            [{}]
-        ); 
-     },
-     pairDevice:function(successCallback, errorCallback, device) {
-        cordova.exec(
-            successCallback,
-            errorCallback, 
-            'LifesenseBLEPlugin',
-            'pairDevice', 
-            [{device:device}]
-        ); 
-        console.log(JSON.stringify(device));
-     },
-     getPairedDevice:function(successCallback, errorCallback) {
-        cordova.exec(
-            successCallback, 
-            errorCallback, 
-            'LifesenseBLEPlugin', 
-            'getPairedDevice',
-            [{}]
-        ); 
-     },
-     askForData:function(successCallback, errorCallback) {
-        cordova.exec(
-            successCallback, 
-            errorCallback, 
-            'LifesenseBLEPlugin', 
-            'askForData',
-            [{}]
-        ); 
-     },
-     askForDataByDeviceMacAddress:function(successCallback, errorCallback, address) {
-        cordova.exec(
-            successCallback, 
-            errorCallback, 
-            'LifesenseBLEPlugin', 
-            'askForDataByDeviceMacAddress',
-            [{address:address}]
-        ); 
-     },
-     getData:function(successCallback, errorCallback) {
-        cordova.exec(
-            successCallback, 
-            errorCallback, 
-            'LifesenseBLEPlugin', 
-            'getData',
-            [{}]
-        ); 
-     },
-     clearDeviceList:function(successCallback, errorCallback) {
-        cordova.exec(
-            successCallback, 
-            errorCallback, 
-            'LifesenseBLEPlugin', 
-            'clearDeviceList',
-            [{}]
-        ); 
-     }
+	startScanning: function(successCallback, errorCallback, timeout) {
+		cordova.exec(
+			successCallback, // success callback function
+			errorCallback, // error callback function
+			'LifesenseBLEPlugin', // mapped to our native Java class called "LifesenseBLEPlugin"
+			'startScanning', // with this action name
+			[{timeout:timeout}]
+		); 
+	 },
+	 stopScanning: function(successCallback, errorCallback) {
+		cordova.exec(
+			successCallback, 
+			errorCallback, 
+			'LifesenseBLEPlugin',
+			'stopScanning',
+			[{}]
+		); 
+	 },
+	 pairDevice:function(successCallback, errorCallback, device, timeout) {
+		cordova.exec(
+			successCallback,
+			errorCallback, 
+			'LifesenseBLEPlugin',
+			'pairDevice', 
+			[{device:device,timeout:timeout}]
+		); 
+		console.log(JSON.stringify(device));
+	 },
+	 askForDataByDeviceMacAddress:function(successCallback, errorCallback, address, timeout) {
+		cordova.exec(
+			successCallback, 
+			errorCallback, 
+			'LifesenseBLEPlugin', 
+			'askForDataByDeviceMacAddress',
+			[{address:address,timeout:timeout}]
+		); 
+	 }
+	 // ,
+	 //  getCurrentDevices:function(successCallback, errorCallback) {
+	 //    cordova.exec(
+	 //        successCallback, 
+	 //        errorCallback, 
+	 //        'LifesenseBLEPlugin', 
+	 //        'getCurrentDevices',
+	 //        [{}]
+	 //    ); 
+	 // },
+	 // getPairedDevice:function(successCallback, errorCallback) {
+	 //    cordova.exec(
+	 //        successCallback, 
+	 //        errorCallback, 
+	 //        'LifesenseBLEPlugin', 
+	 //        'getPairedDevice',
+	 //        [{}]
+	 //    ); 
+	 // },
+	 // askForData:function(successCallback, errorCallback) {
+	 //    cordova.exec(
+	 //        successCallback, 
+	 //        errorCallback, 
+	 //        'LifesenseBLEPlugin', 
+	 //        'askForData',
+	 //        [{}]
+	 //    ); 
+	 // },
+	 // getData:function(successCallback, errorCallback) {
+	 //    cordova.exec(
+	 //        successCallback, 
+	 //        errorCallback, 
+	 //        'LifesenseBLEPlugin', 
+	 //        'getData',
+	 //        [{}]
+	 //    ); 
+	 // },
+	 // clearDeviceList:function(successCallback, errorCallback) {
+	 //    cordova.exec(
+	 //        successCallback, 
+	 //        errorCallback, 
+	 //        'LifesenseBLEPlugin', 
+	 //        'clearDeviceList',
+	 //        [{}]
+	 //    ); 
+	 // }
 }
 module.exports = lifesenseBLEPlugin;
